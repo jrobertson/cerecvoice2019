138a0ea42f7d1f7736652d37a0d9163b0770c2fc
James Robertson <digital.robertson@gmail.com>
2023-01-20 07:56:56 +0000

    bug fix: Now works with Ruby 3.2.0; see File.exist?

--- cerecvoice2019.gemspec
+++ cerecvoice2019.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'cerecvoice2019'
-  s.version = '1.0.1'
+  s.version = '1.0.2'
   s.summary = 'Unofficial Cerevoice gem using their text-to-speech API. #tts'
   s.authors = ['James Robertson']
   s.files = Dir['lib/cerecvoice2019.rb']
-  s.add_runtime_dependency('rxfhelper', '~> 1.6', '>=1.6.3')
+  s.add_runtime_dependency('rxfhelper', '~> 1.6', '>=1.6.4')
   s.signing_key = '../privatekeys/cerecvoice2019.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/cerecvoice2019.rb
+++ lib/cerecvoice2019.rb
@@ -24,71 +24,71 @@ end
 class Cerecvoice2019
   include RXFHelperModule
   using ColouredText
-
-  def initialize(accountid: nil, password: nil, voice: 'Heather', config: {},
-                 cache_filepath: 'cache',
+ 
+  def initialize(accountid: nil, password: nil, voice: 'Heather', config: {}, 
+                 cache_filepath: 'cache', 
                  url: 'https://cerevoice.com/rest/rest_1_1.php', debug: false)
 
     @config = ({
-      'method' => 'speakExtended',
+      'method' => 'speakExtended',                
       'accountID' => accountid,
-      'password' => password,
+      'password' => password,                
       'voice' => voice,
       'audioFormat' => 'ogg',
       'sampleRate' => '48000',
       'metadata' => 'True',
       'audio3D' => 'True'
     }).merge config
-
+    
     @url, @cache_filepath, @debug = url, cache_filepath, debug
 
     if @config['accountID'].nil? or @config['password'].nil? then
-      raise Cerecvoice2019Error, 'invalid accountID or password'
-    end
-
+      raise Cerecvoice2019Error, 'invalid accountID or password' 
+    end    
+    
   end
 
   def tts(text='', voice=@config['voice'], out: 'output.' + @config['audioFormat'])
 
-
+    
     audiofile_out = out
     @config['audioFormat'] = File.extname(out)[1..-1]
 
-
+    
     raise Cerecvoice2019Error, 'text empty' if text.empty?
-
+    
     FileUtils.mkdir_p @cache_filepath
-
+    
     h = Digest::MD5.new << text + voice
     filename = File.join(@cache_filepath, h.to_s + '.'+ @config['audioFormat'])
-
-
-    # attempts to find the audio file from a local cache instead of
+    
+    
+    # attempts to find the audio file from a local cache instead of 
     # making a relatively expensive request through the web API
-
-    if not File.exists? filename then
-
+    
+    if not File.exist? filename then
+            
       @config.merge!('text' => CGI.escape(text), 'voice' => voice)
       url = @url + '?' + @config.map {|x| x.join('=')}.join('&')
       puts ('url: ' + url.inspect).debug if @debug
       xml = URI.open(url).read
       puts ('xml: ' + xml.inspect).debug if @debug
-
+      
       doc = Rexle.new xml
       audio_url = doc.root.text('fileUrl')
       puts ('audio_url: ' + audio_url.inspect).debug if @debug
-
-
+      
+      
       puts ('writing file: ' + filename.inspect).debug if @debug
-
+      
       File.open(filename, "wb") do |saved_file|
         # the following "open" is provided by open-uri
         URI.open(audio_url) {|read_file| saved_file.write(read_file.read) }
-      end
+      end      
 
-    end
-
-    FileX.cp filename, audiofile_out
+    end    
+    
+    FileX.cp filename, audiofile_out    
 
   end
 
